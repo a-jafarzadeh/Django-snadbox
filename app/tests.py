a4bffa372f97c7eb147283a49a2a32e0c04b76c1
a-jafarzade <a.jafarzadeh1998@gmail.com>
2023-10-23 09:54:26 +0330

    add two simple test and alter .gitignore

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SimpleTestCase(TestCase):
+    def test_s1(self):
+        self.assertEqual(5*5, 25)
+
+    def test_s2(self):
+        self.assertEqual(5/5, 1)
